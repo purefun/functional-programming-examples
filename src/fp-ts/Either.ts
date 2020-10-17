944a9cf214dcc4a456d7f45a1de2340a131644ed
Nil Lin <nil@lin.am>
2020-10-17 18:25:03 +0800

    Either widen chain

--- src/fp-ts/Either.ts
+++ src/fp-ts/Either.ts
@@ -1,4 +1,5 @@
-import { fromNullable, isRight, left, of, right } from 'fp-ts/Either'
+import { Either, chainW, fromNullable, isRight, left, of, right } from 'fp-ts/Either'
+import {pipe} from 'fp-ts/lib/pipeable'
 
 /**
  * constructor: of, left, right, fromNullable
@@ -19,4 +20,24 @@ it('fromNullable constructor', () => {
 
   const unexpectedEither = toEither(null)
   expect(unexpectedEither).toEqual(left(errMessage))
+})
+
+// see: https://github.com/gcanti/fp-ts/issues/904
+// W suffix means `Widen`
+it('combines with different error types', () => {
+  interface OneError { _tag: 'OneError' }
+  interface TwoError { _tag: 'TwoError' }
+  interface ThreeError { _tag: 'ThreeError' }
+
+  const e1 = (): Either<OneError, string> => left({ _tag: 'OneError' })
+  const e2 = (_x: string): Either<TwoError, boolean> => left({ _tag: 'TwoError' })
+  const e3 = (_x: boolean): Either<ThreeError, string> => left({ _tag: 'ThreeError' })
+
+  pipe(
+    e1(),
+    chainW(e2),
+    chainW(e3),
+    a => a, // a: Either<OneError | TwoError | ThreeError, string>
+  )
+
 })
